547750e09ae1888c1acbc80706e6564251414e30
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 17:43:38 +0900

    モデルクラス群とテーブルの生成

---
+++ lib/filter/doctrine/base/BasePageFormFilter.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Page filter form base class.
+ *
+ * @package    symfonyband
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BasePageFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'title'      => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'body'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'category'   => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'slug'       => new sfWidgetFormFilterInput(),
+    ));
+
+    $this->setValidators(array(
+      'title'      => new sfValidatorPass(array('required' => false)),
+      'body'       => new sfValidatorPass(array('required' => false)),
+      'category'   => new sfValidatorPass(array('required' => false)),
+      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'slug'       => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('page_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Page';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'         => 'Number',
+      'title'      => 'Text',
+      'body'       => 'Text',
+      'category'   => 'Text',
+      'created_at' => 'Date',
+      'updated_at' => 'Date',
+      'slug'       => 'Text',
+    );
+  }
+}
