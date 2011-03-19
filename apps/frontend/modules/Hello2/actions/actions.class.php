8677b8a5ccdab9c49f9e8aad7d17a3432eeed6a6
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 17:28:16 +0900

    Hello2 モジュールを追加し、/Hello2/IndexというURLで「Hello, Hello!」と表示する

---
+++ apps/frontend/modules/Hello2/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Hello2 actions.
+ *
+ * @package    symfonyband
+ * @subpackage Hello2
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class Hello2Actions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
