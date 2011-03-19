7e7b3e92f17a0151f78f0e76cdbfa52e695f46b9
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 17:49:54 +0900

    PageモジュールとInquiryモジュールの雛形を生成

---
+++ apps/frontend/modules/Page/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Page actions.
+ *
+ * @package    symfonyband
+ * @subpackage Page
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PageActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
