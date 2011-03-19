64a1fd303da582cb6a02cbbf257e84992b2c6eea
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 22:30:38 +0900

    リスト4-25 NewsListSuccess.php ファイル

---
+++ apps/frontend/modules/Page/templates/NewsListSuccess.php
@@ -0,0 +1,13 @@
+<h2>お知らせの一覧</h2>
+<?php if (count($pageList)): ?>
+  <ul>
+    <?php foreach ($pageList as $page): ?>
+      <li>
+        <?php echo $page->getDateTimeObject('created_at')->format('m/d') ?>
+        <?php echo link_to($page->getTitle(), 'news_show', array('slug'=>$page->getSlug())) ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php else: ?>
+  お知らせはありません
+<?php endif; ?>
