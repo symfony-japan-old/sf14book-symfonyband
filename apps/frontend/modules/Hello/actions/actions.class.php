8e05e8014154f36799742cc45715f371744685cc
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 17:11:51 +0900

    Helloモジュールを生成

---
+++ apps/frontend/modules/Hello/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Hello actions.
+ *
+ * @package    symfonyband
+ * @subpackage Hello
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class HelloActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
