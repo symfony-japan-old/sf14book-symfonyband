5a3efc71376e54339581fc3026941b2a92bb2f36
hidenorigoto <hidenorigoto@gmail.com>
2011-03-19 21:14:17 +0900

    リスト4-24 ShowSuccess.php ファイル

--- apps/frontend/modules/Page/templates/ShowSuccess.php
+++ apps/frontend/modules/Page/templates/ShowSuccess.php
@@ -1,2 +1,4 @@
-<br />
-<h2>このページのタイトルは、「<?php echo $page->getTitle() ?>」です。</h2>
+<div id="content">
+  <h2><?php echo $page->getTitle() ?></h2>
+  <p><?php echo nl2br($page->getRawValue()->getBody()) ?></p>
+</div>
