78a75b9460778d6f72d523a846b7e8af49cc6c06
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 17:57:44 +0900

    backendのPageモジュールを生成

---
+++ apps/backend/modules/page/lib/pageGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * page module configuration.
+ *
+ * @package    symfonyband
+ * @subpackage page
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pageGeneratorConfiguration extends BasePageGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/page/lib/pageGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * page module helper.
+ *
+ * @package    symfonyband
+ * @subpackage page
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pageGeneratorHelper extends BasePageGeneratorHelper
+{
+}
