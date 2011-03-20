f91def6cd87eb4c265467cd1d223bc121581a6ae
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 20:23:56 +0900

    リスト11-10 Page モジュールのShowSuccess.php テンプレートに著者名の表示を追加

--- apps/frontend/modules/Page/templates/ShowSuccess.php
+++ apps/frontend/modules/Page/templates/ShowSuccess.php
@@ -5,4 +5,5 @@
 <div id="content">
   <h2><?php echo $page->getTitle() ?></h2>
   <p><?php echo nl2br($page->getRawValue()->getBody()) ?></p>
+  <div id="author"><?php echo $page->getAuthor() ?></div>
 </div>
