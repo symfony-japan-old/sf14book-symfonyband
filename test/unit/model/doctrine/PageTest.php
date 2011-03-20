78120ae07b5597d5c03dac48d665dbf253c6c82b
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 20:06:26 +0900

    リスト11-5 Page モデルのユニットテストを修正

--- test/unit/model/doctrine/PageTest.php
+++ test/unit/model/doctrine/PageTest.php
@@ -1,28 +1,31 @@
 <?php
-include dirname(__FILE__).'/../../../bootstrap/unit.php';          // (1)
+include dirname(__FILE__).'/../../../bootstrap/unit.php';
 new sfDatabaseManager($configuration);
-Doctrine_Core::loadData(sfConfig::get('sf_data_dir').'/fixtures'); // (2)
-$test = new lime_test(5);
+Doctrine_Core::loadData(sfConfig::get('sf_data_dir').'/fixtures');
+$test = new lime_test(6);
 
-newsCountCheck($test, 5);                                          // (3)
+newsCountCheck($test, 5);
 
-$title = 'Lime Test Title';
-$body  = 'Lime Test Body';
+$title  = 'Lime Test Title';
+$body   = 'Lime Test Body';
+$author = '団長';
 
 $page = new Page();
 $page->setTitle($title);
 $page->setBody($body);
 $page->setCategory('news');
+$page->setAuthor($author);
 $page->save();
 
 $testPages = PageTable::getInstance()->createQuery('p')
   ->where('p.title = ?', $title)
   ->execute();
 
-if(count($testPages) === 1)                                        // (4)
+if(count($testPages) === 1)
 {
-  $test->is($testPages[0]->getTitle(), $title, 'Titleの保存取得は正常');
-  $test->is($testPages[0]->getBody(),  $body,  'Bodyの保存取得は正常');
+  $test->is($testPages[0]->getTitle(),  $title,  'Titleの保存取得は正常');
+  $test->is($testPages[0]->getBody(),   $body,   'Bodyの保存取得は正常');
+  $test->is($testPages[0]->getAuthor(), $author, 'Authorの保存取得は正常');
 }
 else
 {
@@ -31,7 +34,7 @@ else
 
 newsCountCheck($test, 6);
 
-$page->delete();                                                   // (5)
+$page->delete();
 
 newsCountCheck($test, 5);
 
