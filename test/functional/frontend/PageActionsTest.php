b56185128a0da9ff1be65f4df66a4bb72b34cf5a
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 20:20:37 +0900

    リスト11-9 著者名が表示されていることをテストする

--- test/functional/frontend/PageActionsTest.php
+++ test/functional/frontend/PageActionsTest.php
@@ -6,12 +6,13 @@ $browser->
   get('/page/about')->
 
   with('request')->begin()->
-    isParameter('module', 'Page')->    // (1)
-    isParameter('action', 'Show')->    // (2)
+    isParameter('module', 'Page')->
+    isParameter('action', 'Show')->
   end()->
   with('response')->begin()->
-    isStatusCode(200)->                // (3)
-    isHeader('content-type', 'text/html; charset=utf-8')-> // (4)
-    checkElement('h2', 'Symfony楽団について')->            // (5)
+    isStatusCode(200)->
+    isHeader('content-type', 'text/html; charset=utf-8')->
+    checkElement('h2', 'Symfony楽団について')->
+    checkElement('#author', 'Symfony楽団長')->             // (6)
   end()
 ;
