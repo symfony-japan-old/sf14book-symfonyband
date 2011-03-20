698a684f766e2e0b25caa627cebfee786a5ba9b8
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 17:52:21 +0900

    リスト4-59 admin ディレクトリのbackend.php とbackend_dev.php のパスを修正

--- web/admin/backend_dev.php
+++ web/admin/backend_dev.php
@@ -7,7 +7,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
