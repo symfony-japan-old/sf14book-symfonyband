0fca5ce795ef6af1bbf1102f78315de18cf0528b
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 16:00:45 +0900

    リスト4-56 メール本文用のテンプレート _InquiryBody.php

---
+++ apps/frontend/modules/Inquiry/templates/_InquiryBody.php
@@ -0,0 +1,14 @@
+Symfony楽団ホームページからのお問い合わせがありました。
+
+■お名前
+<?php echo $name ?>
+
+■メールアドレス
+<?php echo $email ?>
+
+■お問い合わせ内容
+<?php echo $body ?>
+
+■入団希望
+<?php echo InquiryForm::$joinChoices[$join] ?>
+
