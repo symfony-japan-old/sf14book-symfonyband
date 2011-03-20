e9309087ae46b9f7770f95fbc9f618922898edc8
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 13:54:12 +0900

    リスト4-45 Inquiry モジュールのNewSuccess.php テンプレート

--- apps/frontend/modules/Inquiry/templates/NewSuccess.php
+++ apps/frontend/modules/Inquiry/templates/NewSuccess.php
@@ -2,5 +2,9 @@
 <?php echo link_to('Symfony楽団ホームページ', 'homepage') ?> >
 お問い合わせ
 <?php end_slot() ?>
-<h2>お問い合わせフォーム</h2>
-後で実装
+<?php echo $form->renderFormTag(url_for('inquiry_new')) ?>
+<table>
+<?php echo $form; ?>
+</table>
+<input type="submit" name="submit" value="送信" />
+</form>
