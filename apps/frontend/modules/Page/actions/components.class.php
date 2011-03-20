8ec62c0b67e841bcbbaeed8aadde275310014c23
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 18:25:49 +0900

    リスト4-67 page モジュールのcomponents.class.php ファイルのexecuteListLatestNews() メソッドを変更

--- apps/frontend/modules/Page/actions/components.class.php
+++ apps/frontend/modules/Page/actions/components.class.php
@@ -15,9 +15,10 @@ class PageComponents extends sfComponents
   public function executeListLatestNews()
   {
     // カテゴリ名newsに対応するページを5件取得する
-    $this->pageList = PageTable::getInstance()->createQuery('p')
-      ->where('p.category = ?', 'news')
-      ->limit(5)
-      ->execute();
+    $pager = new sfDoctrinePager('Page', 5);
+    $pager->setQuery(PageTable::queryLatestNews());
+    $pager->setPage(1);
+    $pager->init();
+    $this->latestNewsPager = $pager;
   }
 }
