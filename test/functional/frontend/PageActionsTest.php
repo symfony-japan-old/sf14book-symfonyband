244fdd1ea3fc3db374b29021e0c985815e958867
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 20:18:28 +0900

    リスト11-8 Page モジュール用の機能テスト

--- test/functional/frontend/PageActionsTest.php
+++ test/functional/frontend/PageActionsTest.php
@@ -1,19 +1,17 @@
 <?php
-
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
-
 $browser = new sfTestFunctional(new sfBrowser());
 
 $browser->
-  get('/Page/index')->
+  get('/page/about')->
 
   with('request')->begin()->
-    isParameter('module', 'Page')->
-    isParameter('action', 'index')->
+    isParameter('module', 'Page')->    // (1)
+    isParameter('action', 'Show')->    // (2)
   end()->
-
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    isStatusCode(200)->                // (3)
+    isHeader('content-type', 'text/html; charset=utf-8')-> // (4)
+    checkElement('h2', 'Symfony楽団について')->            // (5)
   end()
 ;
