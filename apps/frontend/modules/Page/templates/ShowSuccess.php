fbca0f08bd5823a07f8cdfdbe2e933e1ed13748a
hidenorigoto <hidenorigoto@gmail.com>
2011-03-20 13:38:45 +0900

    リスト4-42 個別のテンプレートからスロットの値を設定(以下はShowSuccess.php)

--- apps/frontend/modules/Page/templates/ShowSuccess.php
+++ apps/frontend/modules/Page/templates/ShowSuccess.php
@@ -1,3 +1,7 @@
+<?php slot('breadcrumb')?>
+<?php echo link_to('Symfony楽団ホームページ', 'homepage') ?> >
+<?php echo $page->getTitle() ?>
+<?php end_slot() ?>
 <div id="content">
   <h2><?php echo $page->getTitle() ?></h2>
   <p><?php echo nl2br($page->getRawValue()->getBody()) ?></p>
